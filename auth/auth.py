9f6eebcaa100e7e6fb3596787b4c1db30a16aa8c
Thomas Chu <itsthomaschu@gmail.com>
2018-03-08 11:20:51 -0700

    created functions for users

--- auth/auth.py
+++ auth/auth.py
@@ -1,9 +1,31 @@
-def canLogin(connection, cursor, username, password):
-    pass
+from hashlib import pbkdf2_hmac
 
+user_types = {
+    'Account Manager': AccountManager,
+    'Supervisor': Supervisor,
+    'Dispatcher': Dispatcher,
+    'Driver': Driver,
+}
+
+def canLogin(connection, cursor, username, password):
+    hash_name = 'sha256'
+    salt = 'ssdirf993lksiqb4'
+    iterations = 100000
+
+    dk = pbkdf2_hmac(hash_name, bytearray(password, 'ascii'), bytearray(salt, 'ascii'), iterations)
+
+    # fetch user with same username
+    cursor.execute("SELECT role, password FROM users WHERE login = ?", username)
+    candid = cursor.fetchone()
+
+    # compare hashed password, if equal create new user
+    if dk == candid[1]:
+        return user_types[candid[0]](connection, cursor, username)
+
+    return None
 
 class Auth:
-    def __init__(self, connection, cursor, username, password):
+    def __init__(self, connection, cursor, username):
         self.username = username
 
     def options(self):
@@ -13,19 +35,43 @@ class Auth:
 
 class AccountManager(Auth):
     role = 'Account Manager'
-    def __init__(self, connection, cursor, username, password):
+    def __init__(self, connection, cursor, username):
         pass
 
+    def getMasterAccount(account_no):
+        pass
+    
+    def addMasterAccount():
+        pass
+
+    def createServiceAgreement(account_no):
+        pass
+    
+    def getSummaryReport(account_no)
+    
+
 class Supervisor(AccountManager):
-    def __init__(self, connection, cursor, username, password):
+    def __init__(self, connection, cursor, username):
+        pass
+
+    def getAccountManagerReport(personell_id):
         pass
 
 class Dispatcher(Auth):
     role = 'Dispatcher'
-    def __init__(self, connection, cursor, username, password):
+    def __init__(self, connection, cursor, username):
+        pass
+
+    def createFulfillment():
         pass
 
 class Driver(Auth):
     role = 'Driver'
-    def __init__(self, connection, cursor, username, password):
+    def __init__(self, connection, cursor, username):
+        pass
+
+    def getTasks():
+        pass
+
+    def getTask():
         pass
