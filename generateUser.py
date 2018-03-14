7de6f875810a046a376cb4a61ecd1e38dde2d8bc
Thomas Chu <itsthomaschu@gmail.com>
2018-03-14 16:32:17 -0600

    fixed some stuff

--- generateUser.py
+++ generateUser.py
@@ -13,26 +13,15 @@ def main():
     db_directory = os.path.join(os.path.dirname(__file__), 'data/')
     ctrl_db = Controller(db_directory, 'test.db')
 
-    # insert schema and data into test db
-    readSQL(ctrl_db, os.path.join(db_directory, 'p1-tables.sql'))
-    readSQL(ctrl_db, os.path.join(db_directory, 'test_data.sql'))
+    user_id = generateID()
+    role = input('Enter user\'s role: ')
+    login = input('Enter username: ')
+    password = input('Enter password: ')
 
-    createNewUser(ctrl_db, '111110', 'account manager', 'bobby', 'password')
-    createNewUser(ctrl_db, '12345', 'admin', 'owner', 'password')
-    createNewUser(ctrl_db, '1', 'driver', 'test', 'password')
-    createNewUser(ctrl_db, '300', 'dispatcher', 'lol', 'password')
-    createNewUser(ctrl_db, '111111', 'account manager', 'd4nny', 'password')
-    createNewUser(ctrl_db, '222222', 'supervisor', 'thomas', 'password')
+    createNewUser(ctrl_db, user_id, role, login, password)
 
-    #user_id = generateID()
-    #role = input('Enter user\'s role: ')
-    #login = input('Enter username: ')
-    #password = input('Enter password: ')
-
-    #createNewUser(ctrl_db, user_id, role, login, password)
-
-    #ctrl_db.cursor.execute("SELECT user_id, role, login FROM users")
-    #displayQuery(ctrl_db, ctrl_db.cursor.fetchall())
+    ctrl_db.cursor.execute("SELECT user_id, role, login FROM users")
+    displayQuery(ctrl_db, ctrl_db.cursor.fetchall())
 
     ctrl_db.connection.close()
 
